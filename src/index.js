fba63b73eb713ad80a395d075babe0f035119584
Rafał Biżek <49203973+RafalBizek@users.noreply.github.com>
2023-08-20 12:45:53 +0200

    features/phonebook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+// import { createRoot } from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import { App } from './components/App';
+
+// import { store } from 'redux/store';
+
+// const rootElement = document.getElementById('root');
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// );
