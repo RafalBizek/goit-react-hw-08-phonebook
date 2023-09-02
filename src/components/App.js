cb08d4a6782a178de7c3e736d60863fffbbf5bba
Rafał Biżek <49203973+RafalBizek@users.noreply.github.com>
2023-09-02 13:08:23 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ export const App = () => {
 
   return (
     <Routes>
-      <Route path="/ element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route
           path="register"
