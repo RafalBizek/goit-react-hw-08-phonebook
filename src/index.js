2beaef9241bb5b0784cfbfd7128d5339bc66b07c
Rafał Biżek <49203973+RafalBizek@users.noreply.github.com>
2023-09-02 13:52:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter basename="https://github.com/RafalBizek/goit-react-hw-08-phonebook">
+    <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
